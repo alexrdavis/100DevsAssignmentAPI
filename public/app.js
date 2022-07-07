883eccc9ca6573cc5e7b2f5de8dc1defe8fd4efe
Alex <alexandradavis13@gmail.com>
2022-07-06 17:31:02 -0700

    updated app.js link

--- public/app.js
+++ public/app.js
@@ -4,5 +4,5 @@ btn.addEventListener("click", btnFunc);
 
 function btnFunc() {
     let classInput = document.querySelector("#classInput").value
-    window.open(`process.env.PORT/${classInput}`)
+    window.open(`https://simple-100devs-assignment-api.herokuapp.com/homework/${classInput}`)
 }
